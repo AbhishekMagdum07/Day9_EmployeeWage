5d00ce4bb288e2a089b9a0dec3c07e10b8d25b95
Abhishek <abhishek.magdum.07@gmail.com>
2023-03-01 13:29:41 +0530

    Added : Employee present or Absent

---
+++ src/com/bridgelabz_empwage/EmployeeWage.java
@@ -0,0 +1,18 @@
+package com.bridgelabz_empwage;
+
+import java.util.Random;
+public class EmployeeWage {
+    Random random = new Random();
+    void attendance(){
+        int numbers = random.nextInt(2);
+        if (numbers == 0) {
+            System.out.println("Employee is Present");
+        }else {
+            System.out.println("Employee is Absent");
+        }
+    }
+    public static void main(String[] args) {
+        EmployeeWage runner = new EmployeeWage();
+        runner.attendance();
+    }
+}
